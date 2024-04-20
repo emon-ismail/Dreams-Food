a2b18f1a9a8b9930bcbb4a2e60a451cd51be201a
emon-isamil <emonisamil69@gmail.com>
2024-04-20 16:45:30 +0600

    Cart added skeleton

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 const activePage=window.location.pathname;
 console.log(activePage);
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "360px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
