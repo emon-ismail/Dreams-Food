8369acd2539f60ee51d5f6d600bbbd61d4f344ba
Tonmoy-Roy <emonismail69@gmail.com>
2024-04-21 11:57:11 +0600

    done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 const activePage=window.location.pathname;
 console.log(activePage);
 
-/* Set the width of the sidebar to 250px (show it) */
+// JavaScript to open and close the side panel
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "360px";
-  }
+  var screenWidth = window.innerWidth;
+  var maxWidth = 360; // Maximum width for the side panel
   
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
+  // Ensure the width doesn't exceed the maximum width or the screen width
+  var width = Math.min(screenWidth, maxWidth);
+  
+  document.getElementById("mySidepanel").style.width = width + "px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
